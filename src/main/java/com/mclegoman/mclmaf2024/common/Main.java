0c295babba8d423d5d354440f5917c6451bd8410
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-04-03 01:07:47 +1300

    Added title screen customizations.

--- src/main/java/com/mclegoman/mclmaf2024/common/Main.java
+++ src/main/java/com/mclegoman/mclmaf2024/common/Main.java
@@ -2,13 +2,17 @@ package com.mclegoman.mclmaf2024.common;
 
 import com.mclegoman.mclmaf2024.common.registry.*;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main implements ModInitializer {
-	public static Logger logger = LoggerFactory.getLogger("mclmaf2024");
+	public static Logger logger;
+	public static ModContainer modContainer;
 	@Override
 	public void onInitialize() {
+		modContainer = FabricLoader.getInstance().getModContainer("mclmaf2024").isPresent() ? FabricLoader.getInstance().getModContainer("mclmaf2024").get() : null;
 		ComponentTypeRegistry.init();
 		GameEventRegistry.init();
 		SoundEventRegistry.init();
@@ -21,5 +25,8 @@ public class Main implements ModInitializer {
 		ScreenHandlerRegistry.init();
 		LootTablesRegistry.init();
 		EasterEggsRegistry.init();
+	}
+	static {
+		logger = LoggerFactory.getLogger("mclmaf2024");
 	}
 }
